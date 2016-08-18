ceb8736e75ffd6d092b984abb15efb69840b972f
sandy <hbhdyangtengfei@163.com>
2016-08-18 16:52:38 +0800

    script

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+###tmux install script
+###sandy 2016-08-18
+SOFT_PATH=$HOME/.tmux/  
+
+if [ ! -d $SOFT_PATH ] ;then  
+mkdir -p $SOFT_PATH  
+fi  
+
 function die()
 {
     echo "${@}"
     exit 1
 }
 
+install()
+{
 for i in $HOME/.tmux $HOME/.tmux.conf; do
   if [[ ( -e $i ) || ( -h $i ) ]]; then
     echo "${i} has been renamed to ${i}.old"
@@ -13,9 +23,53 @@ for i in $HOME/.tmux $HOME/.tmux.conf; do
   fi
 done
 
-git clone git://github.com/yesmeck/tmuxrc.git $HOME/.tmux \
+#git clone git://github.com/yesmeck/tmuxrc.git $HOME/.tmux \
+git clone git://github.com/hbhdytf/tmux_config.git $SOFT_PATH \
     || die "Could not clone the repository to ${HOME}/.tmux"
 
-ln -s $HOME/.tmux/tmux.conf $HOME/.tmux.conf
+}
 
-echo "Install successfully."
+yesmeck()
+{
+    ln -s $HOME/.tmux/yesmeck/tmux.conf $HOME/.tmux.conf
+    echo "yesmeck install successfully."
+}
+
+gpakosz()
+{
+    ln -s $HOME/.tmux/gpakosz/.tmux.conf $HOME/.tmux.conf
+    cp $HOME/.tmux/gpakosz/.tmux.conf.local $HOME/
+    echo "Install successfully."
+}
+update()
+{
+    echo "update"
+}
+
+###case menu #####  
+  
+case $1 in  
+update )  
+update  
+;;  
+  
+install )  
+    case $2 in
+    "") echo "USAGE:$0 install [yesmeck|gpakosz]";exit;;
+    gpakosz) install; gpakosz;;
+    yesmeck) install; yesmeck;;
+    esac  
+;;  
+  
+start )  
+start  
+;;  
+stop )  
+stop  
+;;  
+  
+* )  
+  
+echo "USAGE:$0 {download or install or start or stop}"
+exit  
+esac  
